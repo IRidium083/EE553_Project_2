d91774843332d1e295f2f5cc132d4b258979533f
Yihan <yhuang101@stevens.edu>
2024-12-08 22:11:34 -0500

    add animation

---
+++ Animation/OpeningAnimation.h
@@ -0,0 +1,13 @@
+#ifndef OPENING_ANIMATION_H
+#define OPENING_ANIMATION_H
+
+#include "Animation.h"
+using namespace std;
+
+class OpeningAnimation : public Animation 
+{
+public:
+    void play(); // Override play method
+};
+
+#endif
