b4cf87c26ba3b37890ca3334c9e79a20b27c3565
Yihan <yhuang101@stevens.edu>
2024-12-10 21:46:06 -0500

    main old

--- main.cpp
+++ main.cpp
@@ -3,26 +3,30 @@
 
 int main()
 {
-    Maze maze = Maze::createMazeByDifficulty("easy");
-    //Maze maze = Maze::createMazeBySize(30,50);
-    int height = maze.getSize().first;
-    int width = maze.getSize().second;
-    World world(height, width);
+    //Maze maze = Maze::createMazeByDifficulty("easy");
+    Maze maze = Maze::createMazeBySize(30,50);
+    //int height = maze.getSize().first;
+    //int width = maze.getSize().second;
+    World world(20, 20);
+    world.debug(true);
+    world.makeTestMap();
     world.makeMist();
     cout << "load maze" << endl;
-    world.loadEmpty(maze.getEmptyCells());
+    //world.loadEmpty(maze.getEmptyCells());
     cout << "empty cell loaded" << endl;
-    world.loadMaze(maze.getMaze());
+    //world.loadMaze(maze.getMaze());
     cout << "maze loaded, size: " << world.getSize().first << "," << world.getSize().second << endl;
     
     world.display();
-    for (int i = 4; i < 8; i++)
+    for (int i = 4; i < 10; i++)
     { // cratrue ID starts from 4
 
-        Creature newCreatre("Monster" + i, i);
-        newCreatre.setVisual('M', 1);
-
-        world.addCreature(newCreatre);
+        Monster newMonster("Monster" + i, i);
+        newMonster.setVisual('M', 1);
+        newMonster.setHipPoint(10);
+        newMonster.setAttack(2);
+        newMonster.setDefence(4);
+        world.addCreature(newMonster);
         cout << i << endl;
     }
     cout << "creature added" << endl;
@@ -34,6 +38,9 @@ int main()
     Player newPlayer;
     newPlayer.setPos(make_pair(2,2));
     newPlayer.setVisual('P', 2);
+    newPlayer.setHipPoint(10);
+    newPlayer.setAttack(3);
+    newPlayer.setDefence(5);
     cout << "Add player" << endl;
     world.addPlayer(newPlayer);
     cout << "Player added" << endl;
