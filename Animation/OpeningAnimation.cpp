f7e975626bcb3c1337974daff5aa4d4be87f3e7c
David <ytsui1@stevens.edu>
2024-12-11 22:33:35 -0500

    add one more line in opening

--- Animation/OpeningAnimation.cpp
+++ Animation/OpeningAnimation.cpp
@@ -11,6 +11,8 @@ void OpeningAnimation::play()
     clearScreen(); // Clear the console screen for a fresh display
     gradualPrint("========== Welcome to the Maze Game ==========", 50); // Print the title with a typing animation
     delay(500); // Short delay for dramatic effect
+    gradualPrint("This game is created by YenJun,Tsui and YiHan Huang", 50);
+    delay(500); // Short delay for dramatic effect
     gradualPrint("A mysterious maze awaits you...", 50); // Print the introduction text
     delay(1000); // Delay before the animation begins
 
