f8e57554fb3bf2964c887fe098af7b5e8e9b3048
YHuang <yhuang101@stevens.edu>
2024-12-11 22:38:04 -0500

    Delete build.sh

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Compile the project
-g++ -std=c++11 main.cpp World/World.cpp World/Player.cpp World/Creature.cpp \
-    Maze/Maze.cpp Animation/AnimationTools.cpp Animation/DeathAnimation.cpp \
-    Animation/OpeningAnimation.cpp Animation/VictoryAnimation.cpp World/Monster.cpp -o project
-
-# Run the program
-./project
